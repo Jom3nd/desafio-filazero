4e4c30d00048f9aa1732df3cbdadcf90a6010abb
Jom3nd <joaomendescruz870@gmail.com>
2026-08-07 15:46:06 -0300

    refactor: centraliza configurações da aplicação

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,1 +1,23 @@
+import logging
+import os
+
+#Configuração da automação
 VIACEP_BASE_URL = "https://viacep.com.br/ws"
+
+#Configuração dos diretórios
+OUTPUT_DIR = "output"
+LOG_DIR = "logs"
+
+os.makedirs(OUTPUT_DIR,exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
+
+
+#Configuração dos logs
+
+logging.basicConfig(
+    filename=f"{LOG_DIR}/app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
