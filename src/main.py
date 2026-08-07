d3837ce087ddb0ec0ada18bacdcfb20ff0ae6368
Jom3nd <joaomendescruz870@gmail.com>
2026-08-07 17:32:11 -0300

    feat: implementa o ponto de entrada da automação

---
+++ src/main.py
@@ -0,0 +1,53 @@
+import json
+from dataclasses import asdict
+from config.settings import OUTPUT_DIR
+from services.provisionamento_service import ProvisionamentoService
+
+
+def main():
+
+    resultado = ProvisionamentoService().processar("data/unidades.csv")
+
+    unidades_json = [
+        asdict(unidade)
+        for unidade in resultado["unidades"]
+    ]
+
+    with open(
+        f"{OUTPUT_DIR}/provisionamento.json",
+        "w",
+        encoding="utf-8"
+    ) as arquivo:
+        json.dump(unidades_json, arquivo, ensure_ascii=False, indent=4)
+
+    relatorio = {
+            "sucesso": len(resultado["unidades"]),
+            "falhas": len(resultado["falhas"]),
+            "errors": resultado["falhas"]
+        }
+    
+    with open(
+        f"{OUTPUT_DIR}/relatorio.json"
+        , "w", encoding="utf-8"
+    ) as arquivo:
+        json.dump(relatorio, arquivo, ensure_ascii=False, indent=4)    
+
+    print("==========RESUMO DO PROVISIONAMENTO==========")
+    print(f"Unidades processadas com sucesso: {len(resultado['unidades'])}")
+    print(f"Falhas: {len(resultado['falhas'])}")
+
+    if resultado["falhas"]:
+
+        print("\nFalhas encontradas:")
+
+        for falha in resultado["falhas"]:
+            print(
+                f"- {falha['unidade']}: {falha['erro']}"
+            )
+
+    else:
+        print("\nTodas as unidades foram processadas com sucesso!")
+
+
+if __name__ == "__main__":
+    main()
