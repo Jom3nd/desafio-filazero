f6767b3cef1baac7a3ac2358820a82dd9e4ce316
Jom3nd <joaomendescruz870@gmail.com>
2026-08-08 13:16:09 -0300

    test: implementa asserts nos testes

--- tests/test_provisionamento.py
+++ tests/test_provisionamento.py
@@ -1,5 +1,21 @@
+import csv
+
 from src.services.provisionamento_service import ProvisionamentoService
 
+
 def test_processar():
+    caminho = "data/unidades.csv"
+
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        quantidade_unidades = sum(1 for _ in csv.DictReader(arquivo))
+
     ps = ProvisionamentoService()
-    resultado = ps.processar("data/unidades.csv")
+    resultado = ps.processar(caminho)
+
+    assert "unidades" in resultado
+    assert "falhas" in resultado
+
+    assert (
+        len(resultado["unidades"]) + len(resultado["falhas"])
+        == quantidade_unidades
+    )
